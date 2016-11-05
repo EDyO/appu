17e429c9741504a8dc858459ca45f54ba466717e
dacacioa <dacacioa@gmail.com>
2016-11-05 15:38:40 +0100

    Code fix and readme improvements.

--- work_podcast.py
+++ work_podcast.py
@@ -1,17 +1,27 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-podcast = AudioSegment.from_mp3("/home/mitch/audio_tmp/podcast.mp3")
-song =  AudioSegment.from_mp3("/home/mitch/audio_tmp/intro.mp3")
+print "Importing podcast"
+podcast = AudioSegment.from_mp3("/home/mitch/AAE/podcast.mp3")
 
+print "Importing music"
+song =  AudioSegment.from_mp3("/home/mitch/AAE/intro.mp3")
+
+print "Generating opening music"
+opening = song[:20000]
+
+
+print "Generating final music"
+ending = song[-40000:]
+
+print "Normalizing podcast audio"
 podcast = podcast.normalize()
 #podcast = split_on_silence(podcast) <-- TODO
 
-
-opening = song[:20000]
-
-ending = song[-40000:]
+print "Generating final podcast file: opening + podcast + ending"
 
 final = opening.append(podcast, crossfade=1000)
 final = final.append(ending,  crossfade=4000)
-final.export("EDyO-19.mp3", format="mp3", parameters=["-ac", "1", "-ab", "48k", "-i", "logo.jpg"])
+final.export("EDyO-19.mp3", format="mp3", parameters=["-ac", "1", "-ab", "48k"])
+
+print "Done"
