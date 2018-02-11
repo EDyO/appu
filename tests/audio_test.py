7604cea2c17adb2f6598feafd88b3c33bcdd8bdb
Ignasi Fosch <natx@y10k.ws>
2018-02-11 13:37:58 +0100

    Add tests for `get_jingles`

--- tests/audio_test.py
+++ tests/audio_test.py
@@ -13,6 +13,9 @@ class MockAudioSegment(object):
     def __len__(self):
         return len(self._segment)
 
+    def __getitem__(self, val):
+        return self._segment[val]
+
 def test_load_mp3(monkeypatch):
     original_name = 'original.mp3'
     monkeypatch.setattr(AudioSegment, 'from_mp3', MockAudioSegment.from_mp3)
@@ -25,3 +28,10 @@ def test_load_mp4_fails(monkeypatch):
     with pytest.raises(SystemExit) as sys_exit:
         audio_segment = load_mp3(original_name)
     assert 'The file must have .mp3 extension' in str(sys_exit)
+
+def test_get_jingles(monkeypatch):
+    song_name = 'song.mp3'
+    monkeypatch.setattr(AudioSegment, 'from_mp3', MockAudioSegment.from_mp3)
+    begin, end = get_jingles(song_name)
+    assert len(begin) == 20000
+    assert len(end) == 10000
