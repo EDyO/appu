cc60046c1d98c0648b5090daf7d1d47eabb2669a
Ignasi Fosch <natx@y10k.ws>
2021-09-12 19:37:11 +0200

    Add feedupdater command

--- pkg/appu/appu.go
+++ pkg/appu/appu.go
@@ -1,16 +1,10 @@
 package appu
 
 import (
-	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
-	"time"
-
-	"gopkg.in/ini.v1"
-	"gopkg.in/yaml.v2"
 )
 
 // DownloadFile allows to download a URL content into a file.
@@ -35,80 +29,17 @@ var DownloadFile = func(URL, fileName string) error {
 	return nil
 }
 
+// GetEpisodeLength returns the Content-Length HTTP header value for a URL.
+var GetEpisodeLength = func(URL string) (int, error) {
+	res, err := http.Head(URL)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res.ContentLength), nil
+}
+
 func extractFileNameFromURL(URL string) string {
 	URLPathParts := strings.Split(URL, "/")
 	return strings.Split(URLPathParts[len(URLPathParts)-1], "?")[0]
-}
-
-// Config contains all the configuration data from the YAML file.
-// Part of it is used by the appu container.
-type Config struct {
-	Album          string `yaml:"album"`
-	Artist         string `yaml:"artist"`
-	Bucket         string `yaml:"bucket"`
-	Cover          string `yaml:"cover"`
-	CoverFileName  string
-	Image          string    `yaml:"image"`
-	Intro          string    `yaml:"intro"`
-	Links          []string  `yaml:"links"`
-	Master         string    `yaml:"master"`
-	PubDate        time.Time `yaml:"pubDate"`
-	Summary        string    `yaml:"summary"`
-	Title          string    `yaml:"title"`
-	TrackNo        int       `yaml:"trackNo"`
-	ConfigFileName string
-}
-
-// LoadConfigYAML reads the `YAMLFile` and gets the data loaded in `cfg`, or any error happened in the process.
-func LoadConfigYAML(YAMLFile string) (cfg *Config, err error) {
-	yamlFile, err := ioutil.ReadFile(YAMLFile)
-	if err != nil {
-		return nil, fmt.Errorf("error loading %s: %v ", YAMLFile, err)
-	}
-
-	err = yaml.Unmarshal(yamlFile, &cfg)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling %s: %v", YAMLFile, err)
-	}
-
-	return cfg, nil
-}
-
-// PrepareFiles makes all the configuration ready to be used by the appu container.
-func (c *Config) PrepareFiles() (err error) {
-	c.CoverFileName = extractFileNameFromURL(c.Cover)
-	c.ConfigFileName = "config.cfg"
-
-	err = DownloadFile(c.Cover, c.CoverFileName)
-	if err != nil {
-		return fmt.Errorf("error downloading cover file: %v", err)
-	}
-
-	c.createConfigFile(c.ConfigFileName)
-
-	return nil
-}
-
-func (c *Config) createConfigFile(configFileName string) {
-	cfgINI := ini.Empty()
-	cfgINI.Section("files-config").Key("podcast_file").SetValue(c.Master)
-	cfgINI.Section("files-config").Key("song_file").SetValue(c.Intro)
-	cfgINI.Section("files-config").Key("cover_file").SetValue(fmt.Sprintf("files/%s", c.CoverFileName))
-	cfgINI.Section("files-config").Key("final_file").SetValue(fmt.Sprintf("podcast/%s", c.episodeName()))
-	cfgINI.Section("files-config").Key("podcast_bucket").SetValue(c.Bucket)
-	cfgINI.Section("tag-config").Key("title").SetValue(c.Title)
-	cfgINI.Section("tag-config").Key("artist").SetValue(c.Artist)
-	cfgINI.Section("tag-config").Key("album").SetValue(c.Album)
-	cfgINI.Section("tag-config").Key("track").SetValue(fmt.Sprintf("%d", c.TrackNo))
-	cfgINI.Section("tag-config").Key("year").SetValue(fmt.Sprintf("%d", c.year()))
-	cfgINI.Section("tag-config").Key("comment").SetValue(c.Summary)
-	cfgINI.SaveTo(configFileName)
-}
-
-func (c *Config) episodeName() string {
-	return strings.Replace(extractFileNameFromURL(c.Master), ".master", "", 1)
-}
-
-func (c *Config) year() int {
-	return c.PubDate.Year()
 }
---
+++ pkg/appu/feed.go
@@ -0,0 +1,64 @@
+package appu
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/etree"
+)
+
+// ReadXML loads XML from a file into an `etree.Document` for processing.
+func ReadXML(feedFileName string) (*etree.Document, error) {
+	doc := etree.NewDocument()
+	err := doc.ReadFromFile(feedFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, err
+}
+
+// CreateFeedItem creates a new feed's Item from the Config information.
+func CreateFeedItem(cfg *Config) (*etree.Element, error) {
+	length, err := GetEpisodeLength(cfg.EpisodeURL)
+	if err != nil {
+		return nil, err
+	}
+
+	newEpisodeTag := etree.NewElement("item")
+
+	title := etree.NewElement("title")
+	title.CreateText(cfg.Title)
+	newEpisodeTag.AddChild(title)
+
+	link := etree.NewElement("link")
+	link.CreateText(cfg.EpisodeURL)
+	newEpisodeTag.AddChild(link)
+
+	guid := etree.NewElement("guid")
+	guid.CreateText(cfg.EpisodeURL)
+	newEpisodeTag.AddChild(guid)
+
+	description := etree.NewElement("description")
+	description.CreateText(cfg.Summary)
+	newEpisodeTag.AddChild(description)
+
+	pubDate := etree.NewElement("pubDate")
+	pubDate.CreateText(cfg.PubDate.Format(time.RFC1123Z))
+	newEpisodeTag.AddChild(pubDate)
+
+	enclosure := etree.NewElement("enclosure")
+	enclosure.CreateAttr("length", fmt.Sprintf("%v", length))
+	enclosure.CreateAttr("type", "audio/mpeg")
+	enclosure.CreateAttr("url", cfg.EpisodeURL)
+
+	newEpisodeTag.AddChild(enclosure)
+
+	return newEpisodeTag, nil
+}
+
+// WriteXML creates a new file on disk with the appropriate XML contents from `doc`.
+func WriteXML(doc *etree.Document) {
+	doc.Indent(2)
+	doc.WriteToFile("new_feed.xml")
+}
