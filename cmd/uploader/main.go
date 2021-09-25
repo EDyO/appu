cba2a8203dcf6af1e86bcf5d97c0aab256cc4a10
Ignasi Fosch <natx@y10k.ws>
2021-09-25 02:24:40 +0200

    Get the bucket name from configuration

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -5,21 +5,27 @@ import (
 	"log"
 	"os"
 
+	"github.com/EDyO/appu/pkg/appu"
 	"github.com/EDyO/appu/pkg/aws"
 )
 
 func main() {
 	uploadFileName := os.Args[1]
-	bucket := os.Args[2]
-	destinationKey := os.Args[3]
 	distribution := os.Args[4]
+	destinationKey := os.Args[2]
+	YAMLFileName := os.Args[3]
+
+	cfg, err := appu.LoadConfigYAML(YAMLFileName)
+	if err != nil {
+		log.Fatalf("Error loading config file: %v", err)
+	}
 
 	session, err := aws.GetSession("eu-central-1")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err = aws.UploadFile(session, uploadFileName, bucket, destinationKey); err != nil {
+	if err = aws.UploadFile(session, uploadFileName, cfg.Bucket, destinationKey); err != nil {
 		log.Fatal(err)
 	}
 
