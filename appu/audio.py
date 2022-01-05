ff74f38c1d0c1b0962a35fff6909114dad937225
Ignasi Fosch <natx@y10k.ws>
2022-01-05 05:55:18 +0100

    Make appu accept S3 URLs to download the master
    This removes the problem of getting public URLs. This method uses
    AWS credentials to access the bucket, the same as for uploading the
    resulting audio file.

--- appu/audio.py
+++ appu/audio.py
@@ -1,5 +1,8 @@
 import re
+import logging
 import requests
+import boto3
+from botocore.exceptions import ClientError
 from pydub import AudioSegment
 from pydub.effects import normalize
 
@@ -18,14 +21,45 @@ def download_file(mp3_file_name, file_type):
     return result_file
 
 
+def download_file_from_s3(s3_url, s3_client=None):
+    """Download a file from an S3 bucket
+
+    Arguments:
+    s3_url (string): URL
+    s3_client (boto3.session.Session.client): S3 client to use. If not
+      specified one is created. This is only useful for testing.
+
+    Returns:
+    string: File downloaded
+    """
+
+    # If no S3 client was specified, create a new one.
+    if s3_client is None:
+        s3_client = boto3.client('s3')
+
+    url_parts = s3_url.split("/")
+    bucket, object_parts = url_parts[2], url_parts[3:]
+    object_name = "/".join(object_parts)
+    file_name = "files/{}".format(object_parts[-1])
+
+    # Download the file, catching possible errors.
+    with open(file_name, 'wb') as data:
+        s3_client.download_fileobj(bucket, object_name, data)
+
+    return file_name
+
+
 def load_mp3(mp3_file_name, file_type='podcast'):
     """
     This tries to load the audio from a named mp3 file.
     It checks the filename has mp3 extension.
     """
     url_pattern = re.compile('^http[s]://')
+    s3_pattern = re.compile('^s3://')
     if url_pattern.match(mp3_file_name):
         mp3_file_name = download_file(mp3_file_name, file_type)
+    elif s3_pattern.match(mp3_file_name):
+        mp3_file_name = download_file_from_s3(mp3_file_name)
     if not mp3_file_name.lower().endswith('.mp3'):
         raise SystemExit(
             'Incorrect audio file format. The file must have .mp3 extension'
